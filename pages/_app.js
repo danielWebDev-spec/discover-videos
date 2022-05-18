bd597e846ff444aea97f8886c6a84c9d3f8f1eaa
Daniel <danielwebdeveloper220@gmail.com>
2022-05-18 15:05:40 -0700

    App Route once Logged In

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,21 @@
-import '../styles/globals.css'
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import { magic } from "../lib/magic-client";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const router = useRouter();
+
+  useEffect(async function () {
+    const isLoggedIn = await magic.user.isLoggedIn();
+    if (isLoggedIn) {
+      router.push("/");
+    } else {
+      router.push("/login");
+    }
+  }, []);
+
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
